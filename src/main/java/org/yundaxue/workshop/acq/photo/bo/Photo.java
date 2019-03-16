e47078a122a65e57c98021ffbdcc00af8e0d67c5
ACE <1845972738@qq.com>
2019-03-16 13:57:39 +0800

    编写Photo和LabelPhoto实体类

--- src/main/java/org/yundaxue/workshop/acq/photo/bo/Photo.java
+++ src/main/java/org/yundaxue/workshop/acq/photo/bo/Photo.java
@@ -1,9 +1,21 @@
 package org.yundaxue.workshop.acq.photo.bo;
 
+import java.util.Date;
+
 public class Photo {
     private int photoId;
-    private String photoName;
+    private int type;
+    private long size;
+    private Date uploadTime;
     private int userId;
+    private String photoPath;
+    private String thumbnailPath;
+    private int delState;
+    private String description;
+    private int thumb;
+    private String MD5;
+    private Date delTime;
+    private int BanState;
 
     public int getPhotoId() {
         return photoId;
@@ -13,12 +25,28 @@ public class Photo {
         this.photoId = photoId;
     }
 
-    public String getPhotoName() {
-        return photoName;
+    public int getType() {
+        return type;
     }
 
-    public void setPhotoName(String photoName) {
-        this.photoName = photoName;
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public Date getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setUploadTime(Date uploadTime) {
+        this.uploadTime = uploadTime;
     }
 
     public int getUserId() {
@@ -27,5 +55,69 @@ public class Photo {
 
     public void setUserId(int userId) {
         this.userId = userId;
+    }
+
+    public String getPhotoPath() {
+        return photoPath;
+    }
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
+    }
+
+    public String getThumbnailPath() {
+        return thumbnailPath;
+    }
+
+    public void setThumbnailPath(String thumbnailPath) {
+        this.thumbnailPath = thumbnailPath;
+    }
+
+    public int getDelState() {
+        return delState;
+    }
+
+    public void setDelState(int delState) {
+        this.delState = delState;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getThumb() {
+        return thumb;
+    }
+
+    public void setThumb(int thumb) {
+        this.thumb = thumb;
+    }
+
+    public String getMD5() {
+        return MD5;
+    }
+
+    public void setMD5(String MD5) {
+        this.MD5 = MD5;
+    }
+
+    public Date getDelTime() {
+        return delTime;
+    }
+
+    public void setDelTime(Date delTime) {
+        this.delTime = delTime;
+    }
+
+    public int getBanState() {
+        return BanState;
+    }
+
+    public void setBanState(int banState) {
+        BanState = banState;
     }
 }
