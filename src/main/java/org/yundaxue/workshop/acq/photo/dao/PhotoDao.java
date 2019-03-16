d5e021d719e3ed88f7978b3909529cd183c95d78
lf <1783492844@qq.com>
2019-03-16 13:21:54 +0800

    修改PhotoDao类名

--- src/main/java/org/yundaxue/workshop/acq/photo/dao/PhotoDao.java
+++ src/main/java/org/yundaxue/workshop/acq/photo/dao/PhotoDao.java
@@ -1,12 +1,12 @@
 package org.yundaxue.workshop.acq.photo.dao;
-
+//
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.yundaxue.workshop.acq.photo.bo.Photo;
 import java.util.List;
 
 @Mapper
-public interface PhotoDAO {
+public interface PhotoDao {
     abstract Photo getPhoto(@Param("photoId") int photoId) throws Exception;
     List<Photo> listPhoto(@Param("userId") int userId) throws Exception;
     void insertPhoto(@Param("photo") Photo photo) throws Exception;
--- src/main/java/org/yundaxue/workshop/acq/photo/service/impl/PhotoServiceImpl.java
+++ src/main/java/org/yundaxue/workshop/acq/photo/service/impl/PhotoServiceImpl.java
@@ -2,23 +2,23 @@ package org.yundaxue.workshop.acq.photo.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.yundaxue.workshop.acq.photo.bo.Photo;
-import org.yundaxue.workshop.acq.photo.dao.PhotoDAO;
+import org.yundaxue.workshop.acq.photo.dao.PhotoDao;
 import org.yundaxue.workshop.acq.photo.service.PhotoService;
 
 import java.util.List;
 @Service("PhotoService")
 public class PhotoServiceImpl implements PhotoService {
     @Autowired
-    PhotoDAO photoDAO;
+    PhotoDao photoDao;
 
     @Override
     public Photo getPhoto(int photoId) throws Exception {
-        return photoDAO.getPhoto(photoId);
+        return photoDao.getPhoto(photoId);
     }
 
     @Override
     public List<Photo> listPhoto(int userId) throws Exception {
-        return photoDAO.listPhoto(userId);
+        return photoDao.listPhoto(userId);
     }
 
     @Override
