a0ab35e1adc5a7a0f75546cc972255179d8f6ddb
sunkai016 <767446947@qq.com>
2019-03-17 17:53:07 +0800

    album和albumPhoto表

---
+++ src/main/java/org/yundaxue/workshop/acq/model/Mapper/AlbumPhotoMapper.java
@@ -0,0 +1,26 @@
+package org.yundaxue.workshop.acq.model.Mapper;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import org.yundaxue.workshop.acq.model.AlbumPhoto;
+
+
+@Mapper
+public interface AlbumPhotoMapper {
+	@Insert("insert into album_photo (photo_id,album_id) " +
+			"values(#{photoId},#{albumId})")
+	public void insertAlbumPhoto(int photoId, int albumId);
+
+	@Delete("delete from album_photo where album_photo_id=#{albumPhotoId} and photo_id=#{photoId} and album_id" +
+			"=#{albumId}"
+	)
+	public void deleteAlbum(int albumPhotoId, int photoId, int albumId);
+
+	@Select("select * from album_photo where album_photo_id=#{albumPhotoId} and photo_id=#{photoId} and album_id" +
+			"=#{albumId}")
+	public AlbumPhoto getAlbumPhoto(int albumPhotoId, int photoId, int albumId);
+}
+
---
+++ src/main/java/org/yundaxue/workshop/acq/service/impl/AlbumPhotoServiceImpl.java
@@ -0,0 +1,29 @@
+package org.yundaxue.workshop.acq.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.yundaxue.workshop.acq.model.AlbumPhoto;
+import org.yundaxue.workshop.acq.model.Mapper.AlbumPhotoMapper;
+import org.yundaxue.workshop.acq.service.AlbumPhotoService;
+
+@Service
+public class AlbumPhotoServiceImpl implements AlbumPhotoService {
+	@Autowired
+	AlbumPhotoMapper albumPhotoMapper;
+
+
+	@Override
+	public void insertAlbumPhoto( int photoId, int albumId) throws Exception {
+		albumPhotoMapper.insertAlbumPhoto(photoId, albumId);
+	}
+
+	@Override
+	public void deleteAlbumPhoto(int albumPhotoId, int photoId, int albumId) throws Exception {
+albumPhotoMapper.deleteAlbum(albumPhotoId, photoId, albumId);
+	}
+	@Override
+	public AlbumPhoto getAlbumPhoto(int albumPhotoId, int photoId, int albumId) throws Exception {
+		return albumPhotoMapper.getAlbumPhoto(albumPhotoId, photoId, albumId);
+	}
+}
