0af04d2cc9f53160e6fef6379953b890cd513b23
pioneer <32725220+1041458853@users.noreply.github.com>
2019-03-17 17:22:13 +0800

    Merge branch 'geng'
    # Conflicts:
    #	src/main/java/org/yundaxue/workshop/acq/dao/LabelPhotoDao.java
    #	src/main/java/org/yundaxue/workshop/acq/dao/PhotoDao.java
    #	src/main/java/org/yundaxue/workshop/acq/dao/UserDao.java
    #	src/main/java/org/yundaxue/workshop/acq/service/impl/PhotoServiceImpl.java
    
    修正

---
+++ src/main/java/org/yundaxue/workshop/acq/controller/AdminController.java
@@ -0,0 +1,28 @@
+package org.yundaxue.workshop.acq.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.yundaxue.workshop.acq.dao.UserDao;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by 耿志强 on 2019/3/17.
+ */
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin/login")
+    public String loginGet(Model model) {
+        return "login";
+    }
+
+    @PostMapping("/admin/login")
+    public String loginPost(HttpServletRequest request, UserDao user, Model model, HttpSession httpSession, final RedirectAttributes redirectAttrs) {
+        return null;
+    }
+}
---
+++ src/main/java/org/yundaxue/workshop/acq/controller/DataController.java
@@ -0,0 +1,13 @@
+package org.yundaxue.workshop.acq.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 耿志强 on 2019/3/17.
+ */
+
+@RestController
+public class DataController {
+    
+}
