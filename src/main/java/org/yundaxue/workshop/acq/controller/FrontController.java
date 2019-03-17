e9fc35377463167f5cc7722527908f90c22527c5
pioneer <32725220+1041458853@users.noreply.github.com>
2019-03-17 17:22:13 +0800

    Merge branch 'geng'
    # Conflicts:
    #	src/main/java/org/yundaxue/workshop/acq/dao/LabelPhotoDao.java
    #	src/main/java/org/yundaxue/workshop/acq/dao/PhotoDao.java
    #	src/main/java/org/yundaxue/workshop/acq/dao/UserDao.java
    #	src/main/java/org/yundaxue/workshop/acq/service/impl/PhotoServiceImpl.java
    
    修正

---
+++ src/main/java/org/yundaxue/workshop/acq/controller/FrontController.java
@@ -0,0 +1,14 @@
+package org.yundaxue.workshop.acq.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by 耿志强 on 2019/3/17.
+ */
+@Controller
+public class FrontController {
+
+
+}
