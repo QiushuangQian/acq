eaa87a8e3c746b90f42430ade54dc7d1b3caafb1
qianqiushuang <qiushuangqian@sina.com>
2019-04-21 18:02:04 +0800

    白名单

--- src/main/java/org/yundaxue/workshop/acq/config/WebConfigurer.java
+++ src/main/java/org/yundaxue/workshop/acq/config/WebConfigurer.java
@@ -22,7 +22,7 @@ public class WebConfigurer extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/user/login", "/register","/css/**","/js/**","/user/doLogin"
-        ,"/register/sendMail","/activation");
+        ,"/register/sendMail","/activation","/changePsw/sendMailChangePsw","/changePsw");
         super.addInterceptors(registry);
     }
 }
